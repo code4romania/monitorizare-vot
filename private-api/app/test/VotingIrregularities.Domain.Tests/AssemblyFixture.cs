bf158ffd87c8f469e065c595ad0ec33fe8a192d1
dragos.hont <dragos.hont@gmail.com>
2016-10-11 22:33:53 +0300

    Testing Assembly

---
+++ private-api/app/test/VotingIrregularities.Domain.Tests/AssemblyFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotingIrregularities.Domain.Tests
+{
+    public class AssemblyFixture : IDisposable
+    {
+        public static AssemblyFixture Current = new AssemblyFixture();
+
+        private AssemblyFixture()
+        {
+        }
+
+        ~AssemblyFixture()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ private-api/app/test/VotingIrregularities.Domain.Tests/WarmUpTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VotingIrregularities.Domain.Tests
+{
+    public class WarmUpTests
+    {
+        [Fact]
+        public void PassingTests()
+        {
+            Assert.True(true);
+        }
+    }
+}
