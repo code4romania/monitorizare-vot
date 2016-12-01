41c85b84e06e5e7cf7521d53a42da350ca80e48f
Lidia <necula_lidia@yahoo.com>
2016-12-01 21:29:22 +0200

    Add a FileQueryHandler to upload a file to azure storage

---
+++ private-api/app/src/VotingIrregularities.Api/Queries/FileQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using VotingIrregularities.Api.Services;
+using VotingIrregularities.Api.Models;
+using VotingIrregularities.Domain.FileAggregate;
+using System.Threading.Tasks;
+
+namespace VotingIrregularities.Api.Queries
+{
+    public class FileQueryHandler : IAsyncRequestHandler<ModelFile, AdaugaFileCommand>
+    {
+        IFileService _fileService;
+
+        public FileQueryHandler(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        /// <summary>
+        ///  Uploads a file in azure blob storage
+        /// </summary>
+        /// <returns>The url of the blob</returns>
+        public async Task<AdaugaFileCommand> Handle(ModelFile message)
+        {
+            string url = await _fileService.UploadFromStreamAsync(message.File.OpenReadStream(), message.File.ContentType);
+
+            return new AdaugaFileCommand { Url = url };
+        }
+    }
+}
