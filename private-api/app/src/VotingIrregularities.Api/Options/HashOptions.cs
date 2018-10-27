5dc7eb4edeef38dd39837a8cf221add56c251016
Bogdan <bogdan.vizureanu@gmail.com>
2018-10-27 23:47:24 +0300

    Add `ClearTextService` for pin hashing
    relates to #61
    
    Co-Authored-By: Krzysiek Madejski <krzysztof.madejski@epf.org.pl>

--- private-api/app/src/VotingIrregularities.Api/Options/HashOptions.cs
+++ private-api/app/src/VotingIrregularities.Api/Options/HashOptions.cs
@@ -3,5 +3,16 @@
     public class HashOptions
     {
         public string Salt { get; set; }
+        /// <summary>
+        /// Can be set to `Hash` or `ClearText`
+        /// `Hash` will use the HashService (that needs the Salt setting) to generate hashes for the password
+        /// :warning: `ClearText` will allow your development environment to create and store clear text passwords in the database. Please only use this in development to speed up things.
+        /// </summary>
+        public string ServiceType { get; set; }
+    }
+    public enum HashServiceType
+    {
+        Hash,
+        ClearText
     }
 }
---
+++ private-api/app/src/VotingIrregularities.Api/Services/ClearTextService.cs
@@ -0,0 +1,8 @@
+﻿namespace VotingIrregularities.Api.Services
+{
+    public class ClearTextService : IHashService
+    {
+        public string Salt { get; set; }
+        public string GetHash(string clearString) => clearString;
+    }
+}
