00a47a62ae417038101b446fcdcd968a8c7ec0e2
Bogdan Vizureanu <bogdan.vizureanu@gmail.com>
2019-02-24 16:55:26 +0200

    Remove warning for skipping test

--- src/test/VotingIrregularities.Tests/HashTests.cs
+++ src/test/VotingIrregularities.Tests/HashTests.cs
@@ -24,7 +24,9 @@ namespace VotingIrregularities.Tests
         private IConfigurationRoot Configuration { get; set; }
 
      
+#pragma warning disable xUnit1004 // Test methods should not be skipped
         [Fact(Skip = "Used for generating the password files")]
+#pragma warning restore xUnit1004 // Test methods should not be skipped
         public void SetPasswords()
         {
             var hashOptions = new HashOptions();
