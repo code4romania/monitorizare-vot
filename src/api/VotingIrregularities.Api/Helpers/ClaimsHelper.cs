70b8459a152976ffb93ec1487e62094b65870588
RaduCStefanescu <stefanescuradu@gmail.com>
2019-05-09 15:23:19 +0300

    Fix issue with answers not being saved anymore due to change in property name - the id of the observer was not retrieved from claims anymore

---
+++ src/api/VotingIrregularities.Api/Helpers/ClaimsHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotingIrregularities.Api.Helpers
+{
+    public class ClaimsHelper
+    {
+        public readonly static string _observerIdProperty = "ObserverId";
+        public readonly static string _observerProperty = "Observator";
+
+        public readonly static string _observerDefault = "ONG";
+
+        public readonly static string _genericIdProvider = "Token";
+    }
+}
