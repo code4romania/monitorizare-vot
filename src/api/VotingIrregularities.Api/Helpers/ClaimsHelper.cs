92fd4168133cc52e8df1cdb2aecb6866267e9e41
RaduCStefanescu <stefanescuradu@gmail.com>
2019-05-14 17:08:45 +0300

    Fix issues with polling stations information not being saved - the retrieval of the observer id from claims was not updated to use the new model

--- src/api/VotingIrregularities.Api/Helpers/ClaimsHelper.cs
+++ src/api/VotingIrregularities.Api/Helpers/ClaimsHelper.cs
@@ -9,9 +9,7 @@ namespace VotingIrregularities.Api.Helpers
     {
         public readonly static string _observerIdProperty = "ObserverId";
         public readonly static string _observerProperty = "Observator";
-
         public readonly static string _observerDefault = "ONG";
-
         public readonly static string _genericIdProvider = "Token";
     }
 }
