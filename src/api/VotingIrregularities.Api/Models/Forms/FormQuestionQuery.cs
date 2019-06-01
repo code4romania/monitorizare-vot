de1912578247da0bbebc6a6af46728af02f33147
Trupcea, A. (Andreea) <Andreea.Trupcea@ing.com>
2019-06-01 20:07:42 +0300

    translate form and polling section controllers

---
+++ src/api/VotingIrregularities.Api/Models/Forms/FormQuestionQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using VotingIrregularities.Api.Models.Forms;
+using VotingIrregularities.Domain.Models;
+
+namespace VotingIrregularities.Api.Models
+{
+
+    public class FormQuestionQuery : IRequest<IEnumerable<FormSectionDTO>>
+    {
+        public string FormCode { get; set; }
+        public int CacheHours { get; set; }
+        public int CacheMinutes { get; set; }
+        public int CacheSeconds { get; set; }
+    }
+}
---
+++ src/api/VotingIrregularities.Api/Models/PollingStation/UpdatePollingStationInfo.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.ComponentModel.DataAnnotations;
+using VotingIrregularities.Domain.SectieAggregate;
+
+namespace VotingIrregularities.Api.Models
+{
+    public class UpdatePollingStationInfo {
+        [Required(AllowEmptyStrings = false)]
+        public string CountyCode { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public int IdPollingStation { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public DateTime? LeaveTime { get; set; }
+    }
+
+    public class UpdatePollingStationInfoProfile : Profile
+    {
+        public UpdatePollingStationInfoProfile()
+        {
+            CreateMap<UpdatePollingStationInfo, PollingStationQuery>();
+            CreateMap<UpdatePollingStationInfo, UpdatePollingSectionCommand>();
+        }
+    }
+}
