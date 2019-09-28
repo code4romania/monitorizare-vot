99996e89c8cf535f070c2e6f7b3dd8b89627e2ab
Bogdan Vizureanu <bogdan.vizureanu@gmail.com>
2019-09-28 18:03:18 +0300

    Added auth endpoint for NGOAdmins

---
+++ src/api/VotingIrregularities.Api/Models/AccountViewModels/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+using MonitorizareVot.Ong.Api.Models;
+
+namespace VotingIrregularities.Api.Models.AccountViewModels
+{
+    public class ApplicationUser : IRequest<UserInfo>
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string UserName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Password { get; set; }
+    }
+}
