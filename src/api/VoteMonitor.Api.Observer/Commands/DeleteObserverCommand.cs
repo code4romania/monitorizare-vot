c16f4a415f9c32f132bcaaff8294a7436d9fa76e
Trupcea, A. (Andreea) <Andreea.Trupcea@ing.com>
2019-10-12 12:33:55 +0300

    Implement PUT/DELETE endpoints for Observers.

---
+++ src/api/VoteMonitor.Api.Observer/Commands/DeleteObserverCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace VoteMonitor.Api.Observer.Commands
+{
+    public class DeleteObserverCommand : IRequest<bool>
+    {
+        public int IdObserver { get; set; }
+    }
+}
