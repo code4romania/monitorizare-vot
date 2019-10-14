cf5e0dd04db5db127e72de8152a57985eab27e50
Edward-Cristi Butoiu <butoiu.edwardcristi@outlook.com>
2019-10-15 00:07:38 +0300

    hiding noteid

--- src/api/VoteMonitor.Entities/NoteAttachment.cs
+++ src/api/VoteMonitor.Entities/NoteAttachment.cs
@@ -14,6 +14,7 @@ namespace VoteMonitor.Entities
         [Required, MaxLength(1000)]
         public string NotePath { get; set; }
 
+        [JsonIgnore]
         public int NoteId { get; set; }
 
         [JsonIgnore]
