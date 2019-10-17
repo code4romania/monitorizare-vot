be4c4f81dda354bea903cb0cdaa5dbd24bff08df
Ion Dormenco <idormenco@gmail.com>
2019-10-17 10:02:55 +0300

    implement CRU for option
    Does not implemet Delete because it is complicated to delete an option which  may be used

---
+++ src/api/VoteMonitor.Api.Form/Models/Options/CreateOptionModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VoteMonitor.Api.Form.Models.Options
+{
+	public class CreateOptionModel
+	{
+		[Required]
+		public bool IsFreeText { get; set; }
+
+		[Required(AllowEmptyStrings = false), MaxLength(1000)]
+		public string Text { get; set; }
+
+		public string Hint { get; set; }
+	}
+}
