8c23378ffd73247d537dc7cb335e5a439dbe6f88
Bogdan Vizureanu <bogdan.vizureanu@gmail.com>
2019-10-24 19:57:21 +0300

    tiny clean on `api.notification`

--- src/api/VoteMonitor.Api.Notification/Handlers/NotificationRegDataHandler.cs
+++ src/api/VoteMonitor.Api.Notification/Handlers/NotificationRegDataHandler.cs
@@ -1,65 +1,51 @@
-﻿using MediatR;
-using Microsoft.Extensions.Logging;
-using VoteMonitor.Api.Observer.Commands;
-using VoteMonitor.Api.Core.Services;
-using VoteMonitor.Entities;
-using System.IO;
-using System.Threading.Tasks;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading;
-using System.Collections;
-using System.Collections.Generic;
-using System;
+using System.Threading.Tasks;
+using MediatR;
+using VoteMonitor.Api.Core.Services;
+using VoteMonitor.Api.Notification.Commands;
+using VoteMonitor.Entities;
 
-namespace VoteMonitor.Api.Observer.Handlers
+namespace VoteMonitor.Api.Notification.Handlers
 {
     public class NotificationRegDataHandler :
         IRequestHandler<NotificationRegDataCommand, int>,
         IRequestHandler<NewNotificationCommand, int>
     {
         private readonly VoteMonitorContext _context;
-        private readonly ILogger _logger;
-        private IHashService _hashService;
-        private IFirebaseService _firebaseService;
+        private readonly IFirebaseService _firebaseService;
 
-        public NotificationRegDataHandler(VoteMonitorContext context, ILogger logger, IHashService hashService, IFirebaseService firebaseService)
+        public NotificationRegDataHandler(VoteMonitorContext context, IFirebaseService firebaseService)
         {
             _context = context;
-            _logger = logger;
-            _hashService = hashService;
             _firebaseService = firebaseService;
         }
 
         public Task<int> Handle(NotificationRegDataCommand request, CancellationToken cancellationToken)
         {
-            NotificationRegData notificationReg = new NotificationRegData();
-
-            notificationReg.ObserverId = request.ObserverId;
-            notificationReg.ChannelName = request.ChannelName;
-            notificationReg.Token = request.Token;
+            var notificationReg = new NotificationRegData
+            {
+                ObserverId = request.ObserverId, ChannelName = request.ChannelName, Token = request.Token
+            };
 
             _context.NotificationRegData.Add(notificationReg);
 
-            return _context.SaveChangesAsync();
+            return _context.SaveChangesAsync(cancellationToken);
         }
 
         public Task<int> Handle(NewNotificationCommand request, CancellationToken cancellationToken)
         {
-            List<string> targetFCMTokens = new List<string>();
+            var targetFcmTokens = request.Recipients
+                    .Select(observer => _context.NotificationRegData.AsQueryable()
+                                                                    .Where(regData => regData.ObserverId == int.Parse(observer))
+                    .First(regData => regData.ChannelName == request.Channel))
+                    .Select(regDataResult => regDataResult.Token)
+                    .ToList();
 
-            foreach (string observer in request.Recipients)
-            {
-                var regDataResult = _context.NotificationRegData.AsQueryable().Where(regData => regData.ObserverId == Int32.Parse(observer))
-               .Where(regData => regData.ChannelName == request.Channel).First();
-                targetFCMTokens.Add(regDataResult.Token);
-            }
+            var response = 0;
 
-            int response = 0;
-
-            if(targetFCMTokens.Count > 0)
-            {
-                response = _firebaseService.SendAsync(request.From, request.Title, request.Message, targetFCMTokens);
-            }            
+            if(targetFcmTokens.Count > 0)
+                response = _firebaseService.SendAsync(request.From, request.Title, request.Message, targetFcmTokens);
 
             return Task.FromResult(response);
         }
