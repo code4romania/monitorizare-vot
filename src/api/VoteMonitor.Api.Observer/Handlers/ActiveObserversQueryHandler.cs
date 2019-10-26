fdbe3c7074d62cb5e02496bc1e1e0425756a19d6
Bogdan Vizureanu <bogdan.vizureanu@gmail.com>
2019-10-26 16:46:34 +0300

    initial version of active observers endpoint

---
+++ src/api/VoteMonitor.Api.Observer/Handlers/ActiveObserversQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VoteMonitor.Api.Observer.Models;
+using VoteMonitor.Api.Observer.Queries;
+using VoteMonitor.Entities;
+
+namespace VoteMonitor.Api.Observer.Handlers
+{
+    public class ActiveObserversQueryHandler : IRequestHandler<ActiveObserversQuery, List<ObserverModel>>
+    {
+        private readonly VoteMonitorContext _context;
+        private readonly ILogger _logger;
+        public ActiveObserversQueryHandler(VoteMonitorContext context, ILogger logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public Task<List<ObserverModel>> Handle(ActiveObserversQuery request, CancellationToken cancellationToken)
+        {
+            var results = _context.PollingStationInfos
+                .Include(pi => pi.PollingStation)
+                .Include(pi => pi.PollingStation.County)
+                .Include(pi => pi.Observer)
+                .Where(i => request.CountyCodes.Contains(i.PollingStation.County.Code))
+                .Where(i => i.PollingStation.Number >= request.FromPollingStationNumber)
+                .Where(i => i.PollingStation.Number <= request.ToPollingStationNumber)
+                .Where(i => i.Observer.IdNgo == request.IdNgo)
+                .Select(i => i.Observer)
+                .Select(Mapper.Map<ObserverModel>)
+                .ToList();
+
+            return Task.FromResult(results);
+        }
+    }
+}
---
+++ src/api/VoteMonitor.Api.Observer/Queries/ActiveObserversQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using VoteMonitor.Api.Observer.Models;
+
+namespace VoteMonitor.Api.Observer.Queries
+{
+    public class ActiveObserversQuery :IRequest<List<ObserverModel>>
+    {
+        public string[] CountyCodes { get; set; }
+        public int FromPollingStationNumber { get; set; }
+        public int ToPollingStationNumber { get; set; }
+        public bool CurrentlyCheckedIn { get; set; }
+        public int IdNgo { get; set; }
+    }
+}
