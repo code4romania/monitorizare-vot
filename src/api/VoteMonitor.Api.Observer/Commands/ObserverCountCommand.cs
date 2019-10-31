71dd9be796f476f028f5943e60fa3585c971aeea
Paul Antal <h2opaulh2o@gmail.com>
2019-10-31 22:56:40 +0200

    Add count method and command

---
+++ src/api/VoteMonitor.Api.Observer/Commands/ObserverCountCommand.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VoteMonitor.Api.Core;
+using VoteMonitor.Api.Observer.Models;
+using VoteMonitor.Api.Observer.Queries;
+
+namespace VoteMonitor.Api.Observer.Commands {
+    public class ObserverCountCommand : IRequest<int> {
+        public int IdNgo { get; set; }
+    }
+}
