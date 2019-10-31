e65701926c392eaaf78519ed15a9f48d8b19fc10
Ion Dormenco <idormenco@gmail.com>
2019-10-31 16:32:09 +0200

    fix failing when querying for all observers

--- src/api/VoteMonitor.Api.Observer/Handlers/ObserverListQueryHandler.cs
+++ src/api/VoteMonitor.Api.Observer/Handlers/ObserverListQueryHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -9,51 +8,61 @@ using Microsoft.Extensions.Logging;
 using VoteMonitor.Api.Core;
 using VoteMonitor.Api.Observer.Commands;
 using VoteMonitor.Api.Observer.Models;
-using VoteMonitor.Api.Observer.Queries;
 using VoteMonitor.Entities;
 
 namespace VoteMonitor.Api.Observer.Handlers
 {
-    public class ObserverListQueryHandler : IRequestHandler<ObserverListCommand, ApiListResponse<ObserverModel>>
-    {
-        private readonly VoteMonitorContext _context;
-        private readonly ILogger _logger;
-        public ObserverListQueryHandler(VoteMonitorContext context, ILogger logger)
-        {
-            _context = context;
-            _logger = logger;
-        }
-        public async Task<ApiListResponse<ObserverModel>> Handle(ObserverListCommand request, CancellationToken cancellationToken)
-        {
-            _logger.LogInformation($"Searching for Observers with the following filters (IdNgo, Name, Phone): {request.IdNgo}, {request.Name}, {request.Number}");
+	public class ObserverListQueryHandler : IRequestHandler<ObserverListCommand, ApiListResponse<ObserverModel>>
+	{
+		private readonly VoteMonitorContext _context;
+		private readonly ILogger _logger;
+		public ObserverListQueryHandler(VoteMonitorContext context, ILogger logger)
+		{
+			_context = context;
+			_logger = logger;
+		}
+		public async Task<ApiListResponse<ObserverModel>> Handle(ObserverListCommand request, CancellationToken cancellationToken)
+		{
+			_logger.LogInformation($"Searching for Observers with the following filters (IdNgo, Name, Phone): {request.IdNgo}, {request.Name}, {request.Number}");
 
-            var observers = _context.Observers.AsQueryable();
-            
-            if (request.IdNgo > 0)
-                observers = observers.Where(o => o.IdNgo == request.IdNgo);
-            if (!string.IsNullOrEmpty(request.Name))
-                observers = observers.Where(o => o.Name.Contains(request.Name));
-            if (!string.IsNullOrEmpty(request.Number))
-                observers = observers.Where(o => o.Phone.Contains(request.Number));
+			IQueryable<Entities.Observer> observers = _context.Observers
+				.Include(o => o.Ngo)
+				.Include(o => o.Notes)
+				.Include(o => o.PollingStationInfos);
 
-            var allObservers = observers
-                .Include(o => o.Ngo)
-                .Include(o => o.Notes)
-                .Include(o => o.PollingStationInfos);
-            var count = await allObservers.CountAsync();
+			if (request.IdNgo > 0)
+				observers = observers.Where(o => o.IdNgo == request.IdNgo);
+			if (!string.IsNullOrEmpty(request.Name))
+				observers = observers.Where(o => o.Name.Contains(request.Name));
+			if (!string.IsNullOrEmpty(request.Number))
+				observers = observers.Where(o => o.Phone.Contains(request.Number));
 
-            var requestedPageObservers = observers
-                .Skip(request.PageSize * (request.Page - 1))
-                .Take(request.PageSize)
-                .Select(Mapper.Map<ObserverModel>)
-                .ToList();
+			var count = await observers.CountAsync(cancellationToken);
 
-            return new ApiListResponse<ObserverModel> {
-                TotalItems = count,
-                Data = requestedPageObservers,
-                Page = request.Page,
-                PageSize = request.PageSize
-            };
-        }
-    }
+			var requestedPageObservers = GetPagedQuery(observers, request.Page, request.PageSize)
+				.ToList()
+				.Select(Mapper.Map<ObserverModel>);
+
+
+			return new ApiListResponse<ObserverModel>
+			{
+				TotalItems = count,
+				Data = requestedPageObservers.ToList(),
+				Page = request.Page,
+				PageSize = request.PageSize
+			};
+		}
+
+		private static IQueryable<Entities.Observer> GetPagedQuery(IQueryable<Entities.Observer> observers, int page, int pageSize)
+		{
+			if (pageSize > 0)
+			{
+				return observers
+					.Skip(pageSize * (page - 1))
+					.Take(pageSize);
+			}
+
+			return observers;
+		}
+	}
 }
