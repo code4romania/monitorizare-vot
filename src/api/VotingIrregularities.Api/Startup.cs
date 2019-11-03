de2871b75db3f922e628b65b7922bbce92e96cb2
trupci <andreea.trupcea@ing.com>
2019-11-03 19:07:33 +0200

    Store notifications in database.

--- src/api/VotingIrregularities.Api/Startup.cs
+++ src/api/VotingIrregularities.Api/Startup.cs
@@ -105,6 +105,9 @@ namespace VotingIrregularities.Api
                 .AddApplicationPart(typeof(StatisticsController).Assembly)
                 .AddControllersAsServices()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<VoteMonitorContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new Info
