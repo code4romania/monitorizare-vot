9e050944d6a07197148ed68401e7115c56c7fd65
Ion Dormenco <idormenco@gmail.com>
2019-11-06 18:34:46 +0200

    fix failing build

--- src/api/VoteMonitor.Api.Location/Handlers/PollingStationAssignmentHandler.cs
+++ src/api/VoteMonitor.Api.Location/Handlers/PollingStationAssignmentHandler.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Options;
+using VoteMonitor.Api.Core.Options;
 using VoteMonitor.Api.Location.Models;
 using VoteMonitor.Api.Location.Queries;
 using VoteMonitor.Api.Location.Services;
