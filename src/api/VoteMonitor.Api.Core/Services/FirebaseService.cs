53d96310642085cee7cb557353eec6f7f0fc7346
Ion Dormenco <idormenco@gmail.com>
2019-11-07 18:14:22 +0200

    add batching for notifications

--- src/api/VoteMonitor.Api.Core/Services/FirebaseService.cs
+++ src/api/VoteMonitor.Api.Core/Services/FirebaseService.cs
@@ -1,43 +1,56 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FirebaseAdmin;
 using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
 
 namespace VoteMonitor.Api.Core.Services
 {
-    public class FirebaseService : IFirebaseService
-    {
-        public int SendAsync(string from, string title, string message, IList<string> recipients)
-        {
-            if (FirebaseApp.DefaultInstance == null)
-            {
-                FirebaseApp.Create(new AppOptions()
-                {
-                    Credential = GoogleCredential.GetApplicationDefault(),
-                });
-            }
+	public class FirebaseService : IFirebaseService
+	{
+		public int SendAsync(string from, string title, string message, List<string> recipients)
+		{
+			if (FirebaseApp.DefaultInstance == null)
+			{
+				FirebaseApp.Create(new AppOptions()
+				{
+					Credential = GoogleCredential.GetApplicationDefault(),
+				});
+			}
 
-            var registrationTokens = recipients as IReadOnlyList<string>;
+			int successCount = 0;
 
-            var message2 = new MulticastMessage()
-            {
-                Tokens = registrationTokens,
-                Data = new Dictionary<string, string>()
-                {
-                    { "title", title },
-                    { "body", message },
-                },
-                Notification = new Notification
-                {
-                    Title = title,
-                    Body = message
-                },
-            };
+			if (recipients == null)
+			{
+				return successCount;
+			}
 
-            var response = FirebaseMessaging.DefaultInstance.SendMulticastAsync(message2);
-            response.Wait();
+			while (recipients.Any())
+			{
+				var registrationTokens = recipients.Take(100) as IReadOnlyList<string>;
+				recipients = recipients.Skip(100).ToList();
 
-            return response.Result.SuccessCount;
-        }
-    }
+				var message2 = new MulticastMessage
+				{
+					Tokens = registrationTokens,
+					Data = new Dictionary<string, string>
+					{
+						{ "title", title },
+						{ "body", message },
+					},
+					Notification = new Notification
+					{
+						Title = title,
+						Body = message
+					},
+				};
+
+				var response = FirebaseMessaging.DefaultInstance.SendMulticastAsync(message2);
+				response.Wait();
+				successCount += response.Result.SuccessCount;
+			}
+
+			return successCount;
+		}
+	}
 }
