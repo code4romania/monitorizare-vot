a74d5f51971e630ab9342b59de482a6fcf5e985e
Bogdan Vizureanu <bogdan.vizureanu@gmail.com>
2019-11-11 21:17:57 +0200

    removed newtonsoft.json package from entities

--- src/api/VoteMonitor.Entities/Form.cs
+++ src/api/VoteMonitor.Entities/Form.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,19 +9,15 @@ namespace VoteMonitor.Entities
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
-		[Required, MaxLength(2), JsonProperty(PropertyName = "code")]
 		public string Code { get; set; }
 
 		[Required]
 		public string Description { get; set; }
 
-		[Required, JsonProperty(PropertyName = "ver")]
 		public int CurrentVersion { get; set; }
 
-		[JsonProperty(PropertyName = "diaspora")]
 		public bool Diaspora { get; set; }
 
-		[JsonIgnore]
 		public bool Draft { get; set; }
 
 		public virtual ICollection<FormSection> FormSections { get; set; }
