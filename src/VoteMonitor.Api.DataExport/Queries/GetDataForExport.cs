7e529fb2669976cda9c10f5341553f263d50f76d
Ion Dormenco <idormenco@gmail.com>
2019-11-12 20:13:54 +0200

    add export data project

---
+++ src/VoteMonitor.Api.DataExport/Queries/GetDataForExport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MediatR;
+using VoteMonitor.Api.DataExport.Model;
+
+namespace VoteMonitor.Api.DataExport.Queries
+{
+	public class GetDataForExport : IRequest<ExportModel>
+	{
+		public int? NgoId { get; }
+		public int? ObserverId { get; }
+		public int? PollingStationNumber { get; }
+		public string County { get; }
+		public DateTime? From { get; }
+		public DateTime? To { get; }
+
+		public GetDataForExport(int? ngoId, int? observerId, int? pollingStationNumber, string county, DateTime? from, DateTime? to)
+		{
+			NgoId = ngoId;
+			ObserverId = observerId;
+			PollingStationNumber = pollingStationNumber;
+			County = county;
+			From = from;
+			To = to;
+		}
+	}
+}
