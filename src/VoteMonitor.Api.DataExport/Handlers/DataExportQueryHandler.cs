ddbbb8d75f5d95e3cd8a7e040613301aa8572241
Ion Dormenco <idormenco@gmail.com>
2019-11-12 20:13:54 +0200

    add export data project

---
+++ src/VoteMonitor.Api.DataExport/Handlers/DataExportQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using VoteMonitor.Api.DataExport.Model;
+using VoteMonitor.Api.DataExport.Queries;
+
+namespace VoteMonitor.Api.DataExport.Handlers
+{
+	public class DataExportQueryHandler: IRequestHandler<GetDataForExport, ExportModel>
+	{
+		public Task<ExportModel> Handle(GetDataForExport request, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
