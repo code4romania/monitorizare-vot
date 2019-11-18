4a271eb36084cd15986661eee3a58cb8b08dec66
Ion Dormenco <idormenco@gmail.com>
2019-11-18 14:04:06 +0200

    Create ExceptionLoggerMiddleware.cs

---
+++ src/api/VotingIrregularities.Api/Middleware/ExceptionLoggerMiddleware.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace VotingIrregularities.Api.Middleware
+{
+    public class ExceptionLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly MvcJsonOptions _jsonOptions;
+        private readonly ILogger<ExceptionLoggerMiddleware> _logger;
+
+        public ExceptionLoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, IOptions<MvcJsonOptions> jsonOptions)
+        {
+            var logger = loggerFactory?.CreateLogger<ExceptionLoggerMiddleware>();
+
+            _next = next;
+            _jsonOptions = jsonOptions.Value;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            var injectedRequestStream = new MemoryStream();
+
+            try
+            {
+                var requestLog =
+                    $"REQUEST HttpMethod: {context.Request.Method}, Path: {context.Request.Path} UserAgent: {context.Request.Headers[HeaderNames.UserAgent]}";
+
+                using (var bodyReader = new StreamReader(context.Request.Body))
+                {
+                    var bodyAsText = bodyReader.ReadToEnd();
+                    if (string.IsNullOrWhiteSpace(bodyAsText) == false)
+                    {
+                        requestLog += $", Body : {bodyAsText}";
+                    }
+
+                    var bytesToWrite = Encoding.UTF8.GetBytes(bodyAsText);
+                    injectedRequestStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+                    injectedRequestStream.Seek(0, SeekOrigin.Begin);
+                    context.Request.Body = injectedRequestStream;
+                }
+
+                _logger.LogTrace(requestLog);
+
+                await _next.Invoke(context);
+            }
+            finally
+            {
+                injectedRequestStream.Dispose();
+            }
+            if (context.Response.HasStarted)
+            {
+                _logger.LogWarning("The response has already started, the http status code middleware will not be executed.");
+                throw ex;
+            }
+
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(new { message = "something wrong happened please try again" }, _jsonOptions.SerializerSettings));
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionLoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionLoggerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
