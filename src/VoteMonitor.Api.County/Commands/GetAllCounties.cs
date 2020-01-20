340e0e7d8636ca54e28d757ce6f24dedc36e257c
Ion Dormenco <idormenco@gmail.com>
2020-01-20 22:00:38 +0200

    implement county crud

---
+++ src/VoteMonitor.Api.County/Commands/GetAllCounties.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+using MediatR;
+using VoteMonitor.Api.County.Models;
+
+namespace VoteMonitor.Api.County.Commands
+{
+    public class GetAllCounties : IRequest<Result<List<CountyModel>>>
+    {
+    }
+}
---
+++ src/VoteMonitor.Api.County/Commands/UpdateCounty.cs
@@ -0,0 +1,18 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+using VoteMonitor.Api.County.Models;
+
+namespace VoteMonitor.Api.County.Commands
+{
+    public class UpdateCounty : IRequest<Result>
+    {
+        public CountyModel County { get; }
+        public int CountyId { get; }
+
+        public UpdateCounty(int countyId, CountyModel county)
+        {
+            CountyId = countyId;
+            County = county;
+        }
+    }
+}
