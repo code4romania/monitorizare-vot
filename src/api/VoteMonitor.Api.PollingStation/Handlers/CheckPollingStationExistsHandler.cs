1358783abaad33171288288a637566ea18cdaa64
decembrya <31412211+decembrya@users.noreply.github.com>
2020-02-22 12:55:28 +0200

    add station info controller

---
+++ src/api/VoteMonitor.Api.PollingStation/Handlers/CheckPollingStationExistsHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using VoteMonitor.Api.PollingStation.Queries;
+using VoteMonitor.Entities;
+
+namespace VoteMonitor.Api.PollingStation.Handlers
+{
+    public class CheckPollingStationExistsHandler: IRequestHandler<CheckPollingStationExists, bool>
+    {
+        private readonly VoteMonitorContext _context;
+        private readonly ILogger _logger;
+
+        public CheckPollingStationExistsHandler(VoteMonitorContext context, ILogger logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<bool> Handle(CheckPollingStationExists request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await _context.PollingStations.AnyAsync(p => p.Id == request.Id, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error retrieving polling station: ", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/test/VoteMonitor.Api.PollingStation.Tests/Handlers/CreatePollingStationInfoHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Moq;
+using VoteMonitor.Api.PollingStation.Handlers;
+using VoteMonitor.Api.PollingStation.Profiles;
+using VoteMonitor.Api.PollingStation.Queries;
+using VoteMonitor.Entities;
+using Xunit;
+
+namespace VoteMonitor.Api.PollingStation.Tests.Handlers
+{
+    public class CreatePollingStationInfoHandlerTests
+    {
+        private readonly DbContextOptions<VoteMonitorContext> _dbContextOptions;
+        private readonly MapperConfiguration _mapperConfiguration;
+        private readonly Mock<ILogger> _mockLogger;
+
+        public CreatePollingStationInfoHandlerTests()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<VoteMonitorContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            _mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<PollingStationInfoProfile>();
+            });
+
+            _mockLogger = new Mock<ILogger>();
+        }
+
+        [Fact]
+        public async Task Handler_CreatesPollingStationInfo()
+        {
+            using (var context = new VoteMonitorContext(_dbContextOptions))
+            {
+                var sut = new CreatePollingStationInfoHandler(context, new Mapper(_mapperConfiguration), _mockLogger.Object);
+
+                var createPollingStationInfo = new CreatePollingStationInfo()
+                {
+                    IdPollingStation = 3
+                };
+                await sut.Handle(createPollingStationInfo, new CancellationToken());
+
+                var savedPollingStationInfo = context.PollingStationInfos.FirstOrDefault(p => p.IdPollingStation == createPollingStationInfo.IdPollingStation);
+                savedPollingStationInfo.Should().NotBeNull();
+            }
+
+        }
+    }
+}
