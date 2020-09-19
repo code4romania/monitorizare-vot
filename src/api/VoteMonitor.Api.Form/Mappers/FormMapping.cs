5cda8a93b31011d111bed9015abb1b73e15fdfbd
Irina Borozan <anirib@gmail.com>
2020-09-19 15:55:20 +0300

    added delete form endpoint

---
+++ src/api/VoteMonitor.Api.Form/Mappers/FormMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using VoteMonitor.Api.Form.Queries;
+
+namespace VoteMonitor.Api.Form.Mappers
+{
+    public class FormMapping : Profile
+    {
+        public FormMapping()
+        {
+            _ = CreateMap<DeleteFormModel, DeleteFormCommand>()
+                .ForMember(dest => dest.FormId, c => c.MapFrom(src => src.FormId))
+               ;
+        }
+    }
+}
---
+++ src/api/VoteMonitor.Api.Form/Queries/DeleteFormQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace VoteMonitor.Api.Form.Queries
+{
+    public class DeleteFormModel
+    {
+        public int FormId { get; set; }
+    }
+
+    public class DeleteFormCommand : IRequest<bool>
+    {
+        public int FormId { get; set; }
+    }
+}
