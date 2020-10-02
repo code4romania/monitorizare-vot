6621fca812bae8b47be03844501b906f4cf0ee7e
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-10-02 08:40:34 +0300

    Add get polling station by ID endpoint

--- src/api/VoteMonitor.Api.PollingStation/Controllers/PollingStationV2Controller.cs
+++ src/api/VoteMonitor.Api.PollingStation/Controllers/PollingStationV2Controller.cs
@@ -11,7 +11,7 @@ using VoteMonitor.Api.PollingStation.Queries;
 namespace VoteMonitor.Api.PollingStation.Controllers
 {
     /// <summary>
-    /// Controller responsible for interacting with the polling stations - PollingStationInfo 
+    /// Controller responsible for interacting with the polling stations - PollingStationInfo
     /// </summary>
     [Route("api/v2/polling-station")]
     public class PollingStationV2Controller : Controller
@@ -25,6 +25,7 @@ namespace VoteMonitor.Api.PollingStation.Controllers
             _mapper = mapper;
         }
 
+        /// Retrieves all of the polling stations matching a certain filter.
         [HttpGet]
         [Produces(typeof(IEnumerable<GetPollingStation>))]
         public async Task<IActionResult> GetAllPollingStations([FromQuery]PollingStationsFilter pollingStationsFilter)
@@ -36,9 +37,31 @@ namespace VoteMonitor.Api.PollingStation.Controllers
             return Ok(result);
         }
 
+        /// Retrieves a polling station with the given ID.
+        [HttpGet("{id}")]
+        [Authorize("Organizer")]
+        [Produces(typeof(GetPollingStation))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetPollingStation(int id)
+        {
+            var request = new GetPollingStationById { Id = id };
+
+            var result = await _mediator.Send(request);
+            if (result == null)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(result);
+        }
+
+        /// Updates a polling station's details.
         [HttpPut("{id}")]
         [Authorize("Organizer")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> EditPollingStation([FromRoute]int id, [FromBody]Models.UpdatePollingStation pollingStation)
         {
