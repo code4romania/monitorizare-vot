8b4d17f921c451c2ab008465edf48ddc5d27dc6f
Davide Zoccarato <dzoccarato@live.it>
2020-10-17 18:40:33 +0200

    Add CSV export using CsvWiter library (like for Country Export API)

---
+++ src/api/VoteMonitor.Api.DataExport/CsvExporter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using CsvHelper;
+
+namespace VoteMonitor.Api.DataExport
+{
+    public interface ICsvGenerator
+    {
+        byte[] Export<T>(IEnumerable<T> exportData, string fileName);
+    }
+
+    public partial class Utility
+    {
+        public const char SEPARATOR = ',';
+        public const string CSV_MEDIA_TYPE = "text/csv"; // ref: https://stackoverflow.com/questions/7076042/what-mime-type-should-i-use-for-csv > https://tools.ietf.org/html/rfc7111
+    }
+
+    public class CsvGenerator : ICsvGenerator
+    {
+        private readonly ILogger _logger;
+
+        public CsvGenerator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public byte[] Export<T>(IEnumerable<T> exportData, string fileName)
+        {
+            if (exportData == null || !exportData.Any())
+                return new byte[0];
+
+
+            using (var mem = new MemoryStream())
+            using (var writer = new StreamWriter(mem))
+            using (var csvWriter = new CsvWriter(writer))
+            {
+                csvWriter.Configuration.HasHeaderRecord = true;
+                csvWriter.Configuration.AutoMap<T>();
+
+                csvWriter.WriteRecords(exportData);
+
+                writer.Flush();
+                return mem.ToArray();
+                //return File(mem.ToArray(), "application/octet-stream", "counties.csv");
+            }
+        }
+    }
+}
---
+++ src/api/VoteMonitor.Api.DataExport/Queries/GenerateCSVFile.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using VoteMonitor.Entities;
+
+namespace VoteMonitor.Api.DataExport.Queries
+{
+    public class GenerateCSVFile : IRequest<byte[]>
+    {
+        public IEnumerable<ExportModel> Data { get; }
+
+        public GenerateCSVFile(IEnumerable<ExportModel> data)
+        {
+            Data = data;
+        }
+    }
+}
