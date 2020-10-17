e487a0366e0c12a626efb7f0d394942d02dccfe9
Davide Zoccarato <dzoccarato@live.it>
2020-10-17 23:16:12 +0200

    cleanup

--- src/api/VoteMonitor.Api.DataExport/CsvExporter.cs
+++ src/api/VoteMonitor.Api.DataExport/CsvExporter.cs
@@ -16,7 +16,6 @@ namespace VoteMonitor.Api.DataExport
 
     public partial class Utility
     {
-        public const char SEPARATOR = ',';
         public const string CSV_MEDIA_TYPE = "text/csv"; // ref: https://stackoverflow.com/questions/7076042/what-mime-type-should-i-use-for-csv > https://tools.ietf.org/html/rfc7111
     }
 
@@ -46,7 +45,6 @@ namespace VoteMonitor.Api.DataExport
 
                 writer.Flush();
                 return mem.ToArray();
-                //return File(mem.ToArray(), "application/octet-stream", "counties.csv");
             }
         }
     }
