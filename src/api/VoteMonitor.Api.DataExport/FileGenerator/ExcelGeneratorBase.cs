07d820108574ab9321815c5aa52f9339947712f7
Davide Zoccarato <dzoccarato@live.it>
2020-10-21 19:07:35 +0200

    Feature/issue 317 data export improvement (#319)
    * add Dapper and CsvHelper packages
    
    * - rewrite export query to reduce data to join
    - use dapper instead of Entity Framework to execute query
    - avoid to materialize resultset as list: attempt to redure operations on this method
    
    * ExportDataQueryHandler return IEnumerable instead of List
    
    * make Utility class partial
    
    * Add CSV export using CsvWiter library (like for Country Export API)
    
    * data export controller export a csv
    
    * cleanup
    
    * add Index on Answer table: IdObserver, CountyCode, PollingStationNumber, LastModified
    
    * remove query builder and ripristinate original query
    
    * Code Review (https://github.com/code4romania/monitorizare-vot/pull/319#discussion_r507587684): moved Interfaces and partial classes to respect https://documentation.help/StyleCop/SA1402.html
    
    * Code Review (https://github.com/code4romania/monitorizare-vot/pull/319#discussion_r507590324): used var instead of object type
    
    * Code Review (https://github.com/code4romania/monitorizare-vot/pull/319#discussion_r507624331): removed redundant db close and try-catch in favour of higer level one
    
    * Remove Id form exported csv file

---
+++ src/api/VoteMonitor.Api.DataExport/FileGenerator/ExcelGeneratorBase.cs
@@ -0,0 +1,79 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VoteMonitor.Api.DataExport.FileGenerator
+{
+    public abstract class ExcelGeneratorBase : IExcelGenerator
+    {
+        protected string _sheetName;
+        protected string _fileName;
+        protected List<string> _headers;
+        protected List<string> _type;
+        protected IWorkbook _workbook;
+        protected ISheet _sheet;
+
+        /// <summary>
+        /// Common Code for the Export
+        /// It creates Workbook, Sheet, Generate Header Cells and returns HttpResponseMessage
+        /// </summary>
+        /// <typeparam name="T">Generic Class Type</typeparam>
+        /// <param name="exportData">Data to be exported</param>
+        /// <param name="fileName">Export File Name</param>
+        /// <param name="appendDateTimeInFileName">Specify if filename should contain when file was generated</param>
+        /// <param name="sheetName">First Sheet Name</param>
+        /// <returns></returns>
+        public byte[] Export<T>(List<T> exportData, string fileName,
+            bool appendDateTimeInFileName = false,
+            string sheetName = ExcelUtility.DEFAULT_SHEET_NAME)
+        {
+            _sheetName = sheetName;
+
+            _fileName = appendDateTimeInFileName
+                ? $"{fileName}_{DateTime.Now.ToString(ExcelUtility.DEFAULT_FILE_DATETIME)}"
+                : fileName;
+
+            #region Generation of Workbook, Sheet and General Configuration
+            _workbook = new XSSFWorkbook();
+            _sheet = _workbook.CreateSheet(_sheetName);
+
+            var headerStyle = _workbook.CreateCellStyle();
+            var headerFont = _workbook.CreateFont();
+            headerFont.IsBold = true;
+            headerStyle.SetFont(headerFont);
+            #endregion
+
+            WriteData(exportData);
+
+            #region Generating Header Cells
+            var header = _sheet.CreateRow(0);
+            for (var i = 0; i < _headers.Count; i++)
+            {
+                var cell = header.CreateCell(i);
+                cell.SetCellValue(_headers[i]);
+                cell.CellStyle = headerStyle;
+                // It's heavy, it slows down your Excel if you have large data                
+                _sheet.AutoSizeColumn(i);
+            }
+            #endregion
+
+            #region Generating and Returning Stream for Excel
+            using (var memoryStream = new MemoryStream())
+            {
+                _workbook.Write(memoryStream);
+
+                return memoryStream.ToArray();
+            }
+            #endregion
+        }
+
+        /// <summary>
+        /// Generic Definition to handle all types of List
+        /// Overrride this function to provide your own implementation
+        /// </summary>
+        /// <param name="exportData"></param>
+        public abstract void WriteData<T>(List<T> exportData);
+    }
+}
---
+++ src/api/VoteMonitor.Api.DataExport/FileGenerator/IExcelGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace VoteMonitor.Api.DataExport.FileGenerator
+{
+    public interface IExcelGenerator
+    {
+        byte[] Export<T>(List<T> exportData, string fileName,
+            bool appendDateTimeInFileName = false, string sheetName = ExcelUtility.DEFAULT_SHEET_NAME);
+    }
+}
