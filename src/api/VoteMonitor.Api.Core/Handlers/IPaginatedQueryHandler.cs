4b031b9f75ac978f1932d6cde9c8d81fe97c7e61
Davide Zoccarato <dzoccarato@live.it>
2020-10-25 01:41:10 +0200

    Add facade extension methods to be able to use GetPagedQuery with generic IQueryable

---
+++ src/api/VoteMonitor.Api.Core/Handlers/IPaginatedQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoteMonitor.Api.Core.Handlers
+{
+    public interface IPaginatedQueryHandler
+    { }
+
+    public static class PaginatedQueryHandlerExtension
+    {
+        public static IQueryable<T> GetPagedQuery<T>(this IPaginatedQueryHandler _, IQueryable<T> entities, int page, int pageSize)
+            where T: class // clud be more restrictive if all entities implements the same interface of base class
+        {
+            if (pageSize > 0)
+            {
+                return entities
+                    .Skip(pageSize * (page - 1))
+                    .Take(pageSize);
+            }
+
+            return entities;
+        }
+    }
+}
