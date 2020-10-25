b9a201b1f132e104b9e6d5f06de5b640c9285027
Ion Dormenco <idormenco@gmail.com>
2020-10-25 18:58:13 +0200

    Count all observers when user is organizer  (#336)
    * when user is an organizer should count all observers

--- src/api/VoteMonitor.Api.Observer/Commands/ObserverCountCommand.cs
+++ src/api/VoteMonitor.Api.Observer/Commands/ObserverCountCommand.cs
@@ -4,6 +4,13 @@ namespace VoteMonitor.Api.Observer.Commands
 {
     public class ObserverCountCommand : IRequest<int>
     {
-        public int IdNgo { get; set; }
+        public int IdNgo { get; }
+        public bool IsCallerOrganizer { get; }
+
+        public ObserverCountCommand(int idNgo, bool isCallerOrganizer)
+        {
+            IdNgo = idNgo;
+            IsCallerOrganizer = isCallerOrganizer;
+        }
     }
 }
