d27884a4df125943220118999a3c10fe668fb01c
Adrian Iftode <adrian.iftode@gmail.com>
2020-11-01 17:09:39 +0200

    Add Api Health checks
    - VotingMonitor health check
    - Redis health check
    - Windows Azure health check
    - Firebase health check
    
    Redis, Windows Azure and Firebase are configured
    to be checked only on specific conditions
    that depend on their configuration settings
    
    Runtime configuration changes are considered
    so if you enable a health check while the
    application is running, then this is executed
    otherwise the health check is tagged as not run
    
    Health Check endpoint /health is customized to
    include specific details for each health check
    
    The health checks are published on Application Insights

---
+++ src/api/VoteMonitor.Api/Extensions/HealthChecks/AzureBlobStorageHealthChecksExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using VoteMonitor.Api.Core.Options;
+
+namespace VoteMonitor.Api.Extensions.HealthChecks
+{
+    public static class AzureBlobStorageHealthChecksExtensions
+    {
+        public static IHealthChecksBuilder AddAzureBlobStorage(this IHealthChecksBuilder builder, string name)
+            => builder.Add(new HealthCheckRegistration(
+                   name,
+                   sp => new AzureBlobStorageHealthCheck(sp.GetService<IOptionsSnapshot<BlobStorageOptions>>()), null, null, null));
+    }
+
+    public class AzureBlobStorageHealthCheck : IHealthCheck
+    {
+        private IOptions<BlobStorageOptions> _storageOptions;
+
+        public AzureBlobStorageHealthCheck(IOptions<BlobStorageOptions> storageOptions)
+        {
+            _storageOptions = storageOptions;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var credentials = new StorageCredentials(_storageOptions.Value.AccountName, _storageOptions.Value.AccountKey);
+                var blobClient = new CloudStorageAccount(credentials, _storageOptions.Value.UseHttps).CreateCloudBlobClient();
+
+                var serviceProperties = await blobClient.GetServicePropertiesAsync(
+                    new BlobRequestOptions(),
+                    operationContext: null,
+                    cancellationToken: cancellationToken);
+
+                var container = blobClient.GetContainerReference(_storageOptions.Value.Container);
+                await container.FetchAttributesAsync();
+
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+            }
+        }
+    }
+}
---
+++ src/api/VoteMonitor.Api/Extensions/HealthChecksConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace VoteMonitor.Api.Extensions
+{
+    public static class HealthChecksConfiguration
+    {
+        public static async Task WriteResponse(HttpContext context, HealthReport result, IWebHostEnvironment env)
+        {
+            context.Response.ContentType = "application/json; charset=utf-8";
+
+            var options = new JsonSerializerOptions
+            {               
+                IgnoreNullValues = true
+            };
+
+            using var stream = new MemoryStream();
+
+            var healthResponse = new
+            {
+                status = result.Status.ToString(),
+                totalDuration = result.TotalDuration.ToString(),
+                entries = result.Entries.Select(e => new
+                {
+                    name = e.Key,
+                    status = e.Value.Status.ToString(),
+                    tags = e.Value.Tags,
+                    description = e.Value.Description,
+                    data = env.IsDevelopment() && e.Value.Data?.Count > 0 ? e.Value.Data : null,
+                    exception = env.IsDevelopment() ? ExtractSerializableExceptionData(e.Value.Exception) : null
+                }).ToList()
+            };
+
+            await JsonSerializer.SerializeAsync(stream, healthResponse, healthResponse.GetType(), options);
+            var json = Encoding.UTF8.GetString(stream.ToArray());
+            
+            await context.Response.WriteAsync(json);
+
+            static object ExtractSerializableExceptionData(Exception exception)
+            {
+                if (exception == null)
+                {
+                    return exception;
+                }
+
+                return new
+                {
+                    type = exception.GetType().ToString(),
+                    message = exception.Message,
+                    stackTrace = exception.StackTrace,
+                    source = exception.Source,
+                    data = exception.Data?.Count > 0 ? exception.Data : null,
+                    innerException = exception.InnerException != null ? ExtractSerializableExceptionData(exception.InnerException) : null
+                };
+            };
+        }
+    }
+}
