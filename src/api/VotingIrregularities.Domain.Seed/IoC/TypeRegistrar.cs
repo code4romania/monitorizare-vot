00c215cfbfd581109b0d005cc0ae0bc1b87ae049
Ion Dormenco <idormenco@gmail.com>
2023-08-16 13:12:54 +0300

    rename files

--- src/api/VotingIrregularities.Domain.Seed/IoC/TypeRegistrar.cs
+++ src/api/VotingIrregularities.Domain.Seed/IoC/TypeRegistrar.cs
@@ -4,33 +4,6 @@ using System;
 
 namespace VotingIrregularities.Domain.Seed.IoC
 {
-    public sealed class TypeResolver : ITypeResolver, IDisposable
-    {
-        private readonly IServiceProvider _provider;
-
-        public TypeResolver(IServiceProvider provider)
-        {
-            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
-        }
-
-        public object Resolve(Type type)
-        {
-            if (type == null)
-            {
-                return null;
-            }
-
-            return _provider.GetService(type);
-        }
-
-        public void Dispose()
-        {
-            if (_provider is IDisposable disposable)
-            {
-                disposable.Dispose();
-            }
-        }
-    }
     public sealed class TypeRegistrar : ITypeRegistrar
     {
         private readonly IServiceCollection _builder;
---
+++ src/api/VotingIrregularities.Domain.Seed/IoC/TypeResolver.cs
@@ -0,0 +1,33 @@
+using Spectre.Console.Cli;
+using System;
+
+namespace VotingIrregularities.Domain.Seed.IoC
+{
+    public sealed class TypeResolver : ITypeResolver, IDisposable
+    {
+        private readonly IServiceProvider _provider;
+
+        public TypeResolver(IServiceProvider provider)
+        {
+            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+        }
+
+        public object Resolve(Type type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            return _provider.GetService(type);
+        }
+
+        public void Dispose()
+        {
+            if (_provider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
