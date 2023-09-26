3d875f23ba11ae30664a66cfd16749236037214b
Ion Dormenco <idormenco@gmail.com>
2023-09-26 10:18:51 +0300

    Add missing migration

--- src/api/VotingIrregularities.Domain.Migrator/Migrations/20230926071737_NewPollingStationsInfo.Designer.cs
+++ src/api/VotingIrregularities.Domain.Migrator/Migrations/20230926071737_NewPollingStationsInfo.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VoteMonitor.Entities;
@@ -11,9 +12,11 @@ using VoteMonitor.Entities;
 namespace VotingIrregularities.Domain.Seed.Migrations
 {
     [DbContext(typeof(VoteMonitorContext))]
-    partial class VoteMonitorContextModelSnapshot : ModelSnapshot
+    [Migration("20230926071737_NewPollingStationsInfo")]
+    partial class NewPollingStationsInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
